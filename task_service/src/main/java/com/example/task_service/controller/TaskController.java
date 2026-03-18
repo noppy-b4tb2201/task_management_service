6c08a8de2e49557ed5ee20256f28cdddf7122333
藤村暢寿 <noppy7313.gmail.com>
2026-03-18 22:45:01 +0900

    20260318 taskcontroller

--- task_service/src/main/java/com/example/task_service/controller/TaskController.java
+++ task_service/src/main/java/com/example/task_service/controller/TaskController.java
@@ -1,6 +1,7 @@
 package com.example.task_service.controller;
 
 import com.example.task_service.dto.request.TaskCreateRequestDto;
+import com.example.task_service.dto.response.CommonResponseDto;
 import com.example.task_service.dto.response.TaskResponseDto;
 import com.example.task_service.service.TaskService;
 import jakarta.validation.Valid;
@@ -23,7 +24,20 @@ public class TaskController {
     //タスク追加コントローラー
     @PostMapping(value = "/add")
     @ResponseStatus(HttpStatus.CREATED)
-    public TaskResponseDto add(@AuthenticationPrincipal UUID userId, @RequestBody @Valid TaskCreateRequestDto request) {
-        return taskService.create(userId, request);
+    public CommonResponseDto<TaskResponseDto> add(@AuthenticationPrincipal UUID userId, @RequestBody @Valid TaskCreateRequestDto request) {
+        TaskResponseDto taskResponseDto = taskService.create(userId, request);
+
+        return CommonResponseDto.success(taskResponseDto, "task added successfully");
     }
+
+    //タスク部分検索コントローラー
+    @PostMapping(value = "/serach")
+    @ResponseStatus(HttpStatus.FOUND)
+    public CommonResponseDto<TaskResponseDto> search(@AuthenticationPrincipal UUID userId, Long id) {
+        TaskResponseDto taskResponseDto = taskService.findById(userId, id);
+
+        return CommonResponseDto.success(taskResponseDto, "Task Found");
+    }
+
+
 }
