890fd0f399359b5c0c35234fcd634dac238859fb
藤村暢寿 <noppy7313.gmail.com>
2026-06-05 16:30:22 +0900

    2026-06-05 ファイル移動

---
+++ batch_service/run_task_batch.bat
@@ -0,0 +1,13 @@
+@echo off
+
+echo ==================
+echo Task Batch Start
+echo ==================
+
+java -jar target\batch_service-0.0.1-SNAPSHOT.jar
+
+echo ==================
+echo Task Batch End
+echo ==================
+
+pause
