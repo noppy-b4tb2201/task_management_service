1eb63748adce2fb2319b76ef3ff5d31c97351d1d
藤村暢寿 <noppy7313.gmail.com>
2026-06-05 16:15:38 +0900

    2026-06-05 ファイル移動

--- batch_service/batch_service/src/main/java/com/example/batch_service/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.batch_service.repository;
-
-import com.example.batch_service.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
